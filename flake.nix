d3b4960c4a7a6bd681011f85022f4bf24fff9b26
ShadowSeick <alcolka@gmail.com>
2025-01-01 13:46:26 +0100

    solve odin raylib not launching

--- flake.nix
+++ flake.nix
@@ -123,8 +123,25 @@
           nativeBuildInputs = with pkgs; [
             gcc
             odin
+            raylib
+            glfw
+            xorg.libX11
+            xorg.libXrandr
+            xorg.libXinerama
+            xorg.libXcursor
+            xorg.libXi
+            xorg.xeyes
+            mesa
+            libglvnd
           ];
+
           shellHook = ''
+            export LD_LIBRARY_PATH=${pkgs.xorg.libX11}/lib:${pkgs.xorg.libXrandr}/lib:${pkgs.xorg.libXinerama}/lib:${pkgs.xorg.libXcursor}/lib:${pkgs.xorg.libXi}/lib:${pkgs.raylib}/lib:${pkgs.mesa}/lib:${pkgs.libglvnd}/lib:$LD_LIBRARY_PATH
+            export LIBGL_ALWAYS_SOFTWARE=1
+            export DISPLAY=:0
+            export XDG_SESSION_TYPE=x11
+            export GDK_BACKEND=wayland
+            export SDL_VIDEODRIVER=wayland
             echo "Odin environment running"
           '';
         };
